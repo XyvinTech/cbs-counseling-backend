b49c1d2bd33f810100d350a756143b72ea8c875b
hello-world-ttj <tijotjoseph003@gmail.com>
2024-12-06 14:35:32 +0530

    #reports

--- src/models/sessionModel.js
+++ src/models/sessionModel.js
@@ -16,7 +16,7 @@ const sessionSchema = mongoose.Schema(
     },
     counsellor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     description: { type: String },
-    report: { type: String },
+    report: [{ type: String }],
     case_details: { type: String },
     reschedule_remark: { type: String },
     cancel_remark: { type: String },
