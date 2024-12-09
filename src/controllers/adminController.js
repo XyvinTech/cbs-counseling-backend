2d42965e5798c138e0b33f5da2cf6cbc75b4b148
anjanavu <anjanavu2000@gmail.com>
2024-12-09 23:01:15 +0530

    #user

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -707,10 +707,9 @@ exports.listController = async (req, res) => {
       const filter = {};
       if (searchQuery) {
         filter.$or = [{ title: { $regex: searchQuery, $options: "i" } }];
+      
       }
-      await Event.updateMany(filter, {
-        $set: { creatorModel: "Admin", creator: req.userId },
-      });
+      
       const event = await Event.find(filter)
         .skip(skipCount)
         .limit(limit)
