c3ff0c9a5d0208204f7aacf1be03d4ccf9627db2
hello-world-ttj <tijotjoseph003@gmail.com>
2025-02-13 11:46:17 +0530

    #server

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ const BASE_PATH = `/api/${API_VERSION}`;
 //* Import database connection module
 require("./src/helpers/connection");
 
+//! Define the absolute path to the frontend build directory
+const frontendBuildPath = "/var/www/html/school-frontend";
+
+// !Serve static files from the frontend build directory
+app.use(express.static(frontendBuildPath));
+
 //? Define a route for the API root
 app.get(BASE_PATH, (req, res) => {
   return responseHandler(
